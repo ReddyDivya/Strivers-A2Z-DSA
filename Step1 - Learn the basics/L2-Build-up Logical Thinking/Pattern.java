f0957ec4f8d6e1c7a2139bf2c295c76c7a9868fa
Divya <divyareddy0205@gmail.com>
2024-04-08 23:33:52 -0700

    Pattern 9

--- Step1 - Learn the basics/L2-Build-up Logical Thinking/Pattern.java
+++ Step1 - Learn the basics/L2-Build-up Logical Thinking/Pattern.java
@@ -62,7 +62,7 @@ public class Pattern{
             */
             for(int j=0; j<=i; j++)
             {
-                System.out.print(i+1);
+                System.out.print(i+1+" ");
             }
             System.out.println();
         }
@@ -102,6 +102,64 @@ public class Pattern{
         }
     }
 
+    static void pattern7(int n)
+    {
+        // This is the outer loop which will loop for the rows.
+        for(int i=0; i<n; i++)
+        {
+            // For printing the spaces before stars in each row
+            for(int j=0; j<n-i-1; j++)
+            {
+                System.out.print(" ");
+            }
+
+            // For printing the stars in each row
+            for(int j=0; j<2*i+1; j++ )
+            {
+                System.out.print("*");
+            }
+
+            // For printing the spaces after the stars in each row
+            for(int j=0; j>n-i-1; j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern8(int n)
+    {
+        // This is the outer loop which will loop for the rows.
+        for(int i=0; i<n; i++)
+        {
+            // For printing the spaces before stars in each row
+            for(int j=0; j<i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            // For printing the stars in each row
+            for(int j=0; j<2*n-(2*i+1); j++ )
+            {
+                System.out.print("*");
+            }
+
+            // For printing the spaces after the stars in each row
+            for(int j=0; j<i; j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern9(int n)
+    {
+        pattern7(5);
+        pattern8(5);
+    }
+
     public static void main(String[] args)
     {
         pattern1(5);
@@ -115,5 +173,11 @@ public class Pattern{
         pattern5(5);
         System.out.println();
         pattern6(5);
+        System.out.println();
+        pattern7(5);
+        System.out.println();
+        pattern8(5);
+        System.out.println();
+        pattern9(5);
     }
 }
